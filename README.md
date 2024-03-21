9cae11972050841cfb4ee55c0376ed1a29efd39c
Dennis Hickox <frozzel@me.com>
2024-03-21 08:18:20 -0400

    testing new long live token for facebook

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Authorization: Bearer OPENAI_API_KEY
 
 
 https://dev.to/fardinabir/fetching-linkedin-user-data-and-sign-in-with-linkedin-using-openid-connect-3kf
+https://medium.com/@evgeni.leonti/post-to-facebook-page-with-nodejs-379e885033de
--- controllers/facebook.js
+++ controllers/facebook.js
@@ -106,7 +106,7 @@ postFacebook = async (req, res) => {
 
 // Schedule the Facebook post
 
-cron.schedule('15 12 * * 1-5', () => {
+cron.schedule('0 23 * * 1-5', () => {
     console.log('Running a task every day at 7PM 23utc');
     postFacebook();
 }, null, true, 'America/New_York');
