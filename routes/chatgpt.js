592855455b2413176603a9b69c8595ac99c47616
Dennis Hickox <frozzel@me.com>
2024-03-22 12:49:28 -0400

    testing dalle 3 api for photo creation

--- routes/chatgpt.js
+++ routes/chatgpt.js
@@ -1,11 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////
 const express = require('express');
 const router = express.Router();
-const {testApi, chatGPT} = require('../controllers/chatgpt.js')
+const {testApi, chatGPT, dallE} = require('../controllers/chatgpt.js')
 
 
 /////////////////////////// use routes ///////////////////////////
 router.get('/test', testApi);
 router.post('/generate-response', chatGPT);
+router.post('/dallE', dallE);
 
 module.exports = router;
