111cc5c7fc7ccc1b77a8d0cd1aae9e594babc518
Dennis Hickox <frozzel@me.com>
2024-03-28 14:29:35 -0400

    baseline api testing complete just need parms sets for articles and crons set up for publishing

--- controllers/news.js
+++ controllers/news.js
@@ -10,3 +10,18 @@ exports.testApi = (req, res) => {
 
 ////////// News API //////////
 
+exports.getNews = async (req, res) => {
+    try {
+
+        const {category} = req.body;
+        console.log('Category', category);
+        console.log('Getting News..........')
+        const news = await axios.get(`https://newsapi.org/v2/everything?q=SEO&domains=techcrunch.com,thenextweb.com,theverge.com,wired.com&apiKey=${process.env.NEWS_API_KEY}`);
+        console.log("News Articles", news.data.articles.length);
+        res.json(news.data.articles);
+    } 
+    catch (error) {
+        console.error(error.message);
+        res.json({error: error.message});
+    }
+};
