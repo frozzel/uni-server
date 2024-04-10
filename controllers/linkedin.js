0375528b3412bdd8592e64c9f78410a3a0022f06
Dennis Hickox <frozzel@me.com>
2024-04-10 00:20:38 -0400

    testing new newsApi

--- controllers/linkedin.js
+++ controllers/linkedin.js
@@ -163,7 +163,7 @@ postLinkedInTechNews = async (req, res) => {
 
 postLinkedInBusNews = async (req, res) => {
     console.log('Getting Tech News..........')
-    const news = await axios.get(`https://newsapi.org/v2/everything?q=+small +business&domains=investing.com,fortune.com,seekingalpha.com&pageSize=100&sortBy=relevancy&apiKey=${process.env.NEWS_API_KEY}`);
+    const news = await axios.get(`https://newsapi.org/v2/everything?q=%2Bsmall+%2Bbusiness+AND+%28Supply+Chain+Disruptions+OR+Tax+Changes+OR+Rising+Costs+OR+Remote+Work+OR+E-commerce%29&pageSize=100&sortBy=relevancy&apiKey=${process.env.NEWS_API_KEY}`);
     console.log("News Articles", news.data.articles.length);
 
 
--- controllers/news.js
+++ controllers/news.js
@@ -15,7 +15,7 @@ exports.getNews = async (req, res) => {
         console.log('Getting News..........')
         // const news = await axios.get(`https://newsapi.org/v2/everything?q=artificial intelligence OR SEO OR Analytics OR CRM OR Digital Marketing&pageSize=10&language=en&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`);
         // const news = await axios.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch,wired,the-next-web&apiKey=${process.env.NEWS_API_KEY}`);
-        const news = await axios.get(`https://newsapi.org/v2/everything?q=+small +business&domains=investing.com,fortune.com,seekingalpha.com&pageSize=100&sortBy=relevancy&apiKey=${process.env.NEWS_API_KEY}`);
+        const news = await axios.get(`https://newsapi.org/v2/everything?q=%2Bsmall+%2Bbusiness+AND+%28Supply+Chain+Disruptions+OR+Tax+Changes+OR+Rising+Costs+OR+Remote+Work+OR+E-commerce%29&pageSize=100&sortBy=relevancy&apiKey=${process.env.NEWS_API_KEY}`);
 
         
         console.log("News Articles", news.data.articles.length);
