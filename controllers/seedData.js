cf94e7763bb60b81b4b8a2c5369348d3ba0d644c
Dennis Hickox <frozzel@me.com>
2024-06-24 23:25:53 -0400

    adjusted post seeds for weekly delivery

--- controllers/seedData.js
+++ controllers/seedData.js
@@ -4,35 +4,35 @@ const post = [
         "image_url":  'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979928/White_Professional_Painting_Services_Flyer_u_-_Made_with_PosterMyWall_hsxpmu.jpg',
         "link": "https://cgipropainters.com"
     },
-    {
-        "message": "Boost your curb appeal with professional exterior painting by CGI Pro Painters. Call us today! #ExteriorPainting #CurbAppeal",
-        "image_url":'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/PAINTING_SERVICES_POSTER_-_Made_with_PosterMyWall_n7gphe.jpg',
-        "link": "https://cgipropainters.com"
-    },
-    {
-        "message": "Revamp your office space with vibrant colors! CGI Pro Painters offers top-notch commercial painting services. #OfficeRenovation #CommercialPainting",
-        "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Interior_design_unlimited_-_Made_with_PosterMyWall_vgm0jf.jpg',
-        "link": "https://cgipropainters.com"
-    },
-    {
-        "message": "Say goodbye to outdated wallpaper! CGI Pro Painters provide seamless wallpaper removal and painting. #WallpaperRemoval #HomeUpgrade",
-        "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Painting_Services_Design_Templates_-_Made_with_PosterMyWall_ofstgh.jpg',
-        "link": "https://cgipropainters.com"
-    },
-    {
-        "message": "From prep to finish, we handle it all! Experience quality and precision with CGI Pro Painters. #QualityPainting #ProfessionalPainters",
-        "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Painting_Services_Design_Templates_-_Made_with_PosterMyWall_1_gdz43l.jpg',
-        "link": "https://cgipropainters.com"
-    },
-    {
-        "message": "Enhance your living space with our custom painting solutions. CGI Pro Painters, your trusted partner in painting! #CustomPainting #InteriorDesign",
-        "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/House_Painting_Services_Flyer_-_Made_with_PosterMyWall_ygam1p.jpg',
-        "link": "https://cgipropainters.com"
-    },
-    {
-        "message": "Bring your vision to life with CGI Pro Painters. We offer personalized painting services tailored to your needs. #PersonalizedService #DreamHome",
-        "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Painting_Service_Flyer_Template_-_Made_with_PosterMyWall_z1twty.jpg',
-        "link": "https://cgipropainters.com"
-    }
+    // {
+    //     "message": "Boost your curb appeal with professional exterior painting by CGI Pro Painters. Call us today! #ExteriorPainting #CurbAppeal",
+    //     "image_url":'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/PAINTING_SERVICES_POSTER_-_Made_with_PosterMyWall_n7gphe.jpg',
+    //     "link": "https://cgipropainters.com"
+    // },
+    // {
+    //     "message": "Revamp your office space with vibrant colors! CGI Pro Painters offers top-notch commercial painting services. #OfficeRenovation #CommercialPainting",
+    //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Interior_design_unlimited_-_Made_with_PosterMyWall_vgm0jf.jpg',
+    //     "link": "https://cgipropainters.com"
+    // },
+    // {
+    //     "message": "Say goodbye to outdated wallpaper! CGI Pro Painters provide seamless wallpaper removal and painting. #WallpaperRemoval #HomeUpgrade",
+    //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Painting_Services_Design_Templates_-_Made_with_PosterMyWall_ofstgh.jpg',
+    //     "link": "https://cgipropainters.com"
+    // },
+    // {
+    //     "message": "From prep to finish, we handle it all! Experience quality and precision with CGI Pro Painters. #QualityPainting #ProfessionalPainters",
+    //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Painting_Services_Design_Templates_-_Made_with_PosterMyWall_1_gdz43l.jpg',
+    //     "link": "https://cgipropainters.com"
+    // },
+    // {
+    //     "message": "Enhance your living space with our custom painting solutions. CGI Pro Painters, your trusted partner in painting! #CustomPainting #InteriorDesign",
+    //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/House_Painting_Services_Flyer_-_Made_with_PosterMyWall_ygam1p.jpg',
+    //     "link": "https://cgipropainters.com"
+    // },
+    // {
+    //     "message": "Bring your vision to life with CGI Pro Painters. We offer personalized painting services tailored to your needs. #PersonalizedService #DreamHome",
+    //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Painting_Service_Flyer_Template_-_Made_with_PosterMyWall_z1twty.jpg',
+    //     "link": "https://cgipropainters.com"
+    // }
 ]
 module.exports = post;
