5507f1eb2a41446710db9897be8c0352a813a6f5
Dennis Hickox <frozzel@me.com>
2024-08-19 12:01:13 -0400

    seed update

--- controllers/seedData.js
+++ controllers/seedData.js
@@ -1,14 +1,14 @@
 const post = [
-    {
-        "message": "Give your home a stunning new look with CGI Pro Painters! Our team ensures a flawless finish every single time. #HomeRenovation #InteriorPainting #FreshLook #ExpertPainters",
-        "image_url":  'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979928/White_Professional_Painting_Services_Flyer_u_-_Made_with_PosterMyWall_hsxpmu.jpg',
-        "link": "https://cgipropainters.com"
-    },
     // {
-    //     "message": "Enhance your home's curb appeal with our expert exterior painting services. Contact CGI Pro Painters today! #ExteriorPainting #CurbAppeal #HouseTransformation #ExpertPainters",
-    //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/PAINTING_SERVICES_POSTER_-_Made_with_PosterMyWall_n7gphe.jpg',
+    //     "message": "Give your home a stunning new look with CGI Pro Painters! Our team ensures a flawless finish every single time. #HomeRenovation #InteriorPainting #FreshLook #ExpertPainters",
+    //     "image_url":  'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979928/White_Professional_Painting_Services_Flyer_u_-_Made_with_PosterMyWall_hsxpmu.jpg',
     //     "link": "https://cgipropainters.com"
     // },
+    {
+        "message": "Enhance your home's curb appeal with our expert exterior painting services. Contact CGI Pro Painters today! #ExteriorPainting #CurbAppeal #HouseTransformation #ExpertPainters",
+        "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/PAINTING_SERVICES_POSTER_-_Made_with_PosterMyWall_n7gphe.jpg',
+        "link": "https://cgipropainters.com"
+    },
     // {
     //     "message": "Refresh your workspace with vibrant colors! CGI Pro Painters provide premium commercial painting solutions. #OfficeMakeover #CommercialPainting #WorkspaceRefresh #BusinessTransformation",
     //     "image_url": 'https://res.cloudinary.com/dxtsuejin/image/upload/v1718979891/Interior_design_unlimited_-_Made_with_PosterMyWall_vgm0jf.jpg',
