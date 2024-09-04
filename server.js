09ef692774f842f080cafb21175c95e2b1fde1c1
Dennis Hickox <frozzel@me.com>
2024-09-04 11:36:57 -0400

    updated testing page

--- server.js
+++ server.js
@@ -40,7 +40,15 @@ const PORT = process.env.PORT || 8080;
 
 /////////////////////// test server running ///////////////////////
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    const date = new Date();
+    res.send(`<body style="background: #333; display: flex">
+        <div style="width: 30%; height: auto"></div>
+        <div style="display: flex-column; position: relative; top: 25%; width: 100%; height: 15%; box-shadow: 0 0 3px 2px #cec7c759; padding: 1em; border-radius: 8px;">
+        <h1 style="text-align: center; color: white;">🚀  Server Running  🚀</h1> \n 
+        <h3 style="text-align: center; color: white">${date.toString().slice(0, 24)}</h3>
+        </div><div style="width: 30%; height: auto"></div>
+        </body>`
+     );
 });
 
 server.listen(PORT,  () => {// start express server on port 8080
